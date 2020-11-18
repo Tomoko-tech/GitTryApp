d3d3c4123bf30e1d795b26c602961943354c2b54
Tomoko-tech <tomoko.takahashi715@gmail.com>
2020-11-18 15:52:52 +0900

    first commit

---
+++ GitTryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTryApp
+//
+//  Created by Takahashi Tomoko on 2020/11/18.
+//  Copyright © 2020 takatomo.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
